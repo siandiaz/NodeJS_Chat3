5eaad0dab6efc0ad16967d30dbbb24f9f3db7a1c
Nelson Diaz <siandiaz@gmail.com>
2013-01-28 23:10:23 -0600

    primer commit

---
+++ main.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+var app = express.createServer();
+var io = require('socket.io').listen(app);
+
+var PORT = process.env.PORT || 8080;
+
+app.listen (PORT, function(){
+	console.log ("Listening on port " + PORT)
+});
+
+app.configure(function(){
+	app.set('view options', {
+		layout: false
+	});
+
+	app.use(express.static(__dirname + '/static'));
+});
+
+io.configure(function(){
+	io.disable('log');
+
+	io.set('transports',['websockets','xhr-pooling']);
+});
+
+
+app.get('/', function(request, response){
+	response.render('main.jade');
+});
+
+require('./io')(io);
